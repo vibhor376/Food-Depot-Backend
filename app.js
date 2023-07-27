eb4bdab0e64df8db71b911bc9357e420458e780f
Vibhor Bhatnagar <vibhorb01@gmail.com>
2023-07-27 19:30:08 +0530

    Changes in env part-2

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(
     cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL,
+        origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
